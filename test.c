31e03494b00106656c4588a779593c922c28a365
He3k_Win10 <Cloud.He3k@yandex.ru>
2020-05-27 04:07:42 +0700

    Add branch priority

--- test.c
+++ test.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
-{
-    printf("%s\n", argv[1]);
-    char* array;
-    array = argv[1];
-    printf("%s\n", array);
-    printf("%c\n", array[1]);
-    printf("%c\n", array[2]);
-    return 0;
-}
+int main(int argc, char *argv[])
+{	
+	printf("%s\n", argv[1]);    // check string               |     A
+	char *array;               // init CHAR array             |     R
+	array = argv[1];          // copy string --> CHAR array   |     R
+	printf("%s\n", array);   // check CHAR array              |     A
+	                        //                                |     Y
+	/*size array --> count && print array*/	
+ 
+	int i = 0;                               //
+	int count = 0;                          //
+	printf("\n-- print_char_array --\n");  //
+	while(array[i] != 0){	              // print CHAR array 
+		printf("%c\n", array[i]);        // 
+		count ++;		    		    //
+		i++;                           //
+	}
+	
+	
+	
