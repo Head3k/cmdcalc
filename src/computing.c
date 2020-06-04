79d16cbb12fc67d1e77a8d97d76ae60026b9947b
He3k_Kubuntu <Nickolablaniy@mail.ru>
2020-06-04 15:43:32 +0700

    Update

--- src/computing.c
+++ src/computing.c
@@ -3,50 +3,50 @@
 #include <stdlib.h>                                 
 #include "include/computing.h"
  
-void delete_space(char *array)
+void delete_space(char *array)                           // delete space
 {
-    int i = 0, j = 0;
+    int i = 0, j = 0;                                  // i & j  for cycle 
     
-    while ((*(array+i) = *(array + j++)) != '\0') {
-        if (*(array+i) != ' ') {
+    while ((*(array+i) = *(array + j++)) != '\0') {  // matching elements to the end array
+        if (*(array+i) != ' ') {                    // search ' '
             i++;
         }
     }
 }
 
-void check_brackets(char *array)
-{
-    int open = 0, close = 0;
-    
-    while (*array) {
-        if (*array == '(') {
-            open++;
-        } else if (*array == ')') {
-            close++;
-        }
-   
-        ++array;
+void check_brackets(char *array)                          // check error brackets
+{                                          
+    int open = 0, close = 0;                            // count open & close brackets
+                                         
+    while (*array) {                                  // matching elements to the end array
+        if (*array == '(') {                         // check open brackets
+            open++;                                 // count open brackets
+        } else if (*array == ')') {                // check close brackets
+            close++;                              // count close brackets
+        }                         
+                                  
+        ++array;                               // next element array
     }
   
-    if (open != close) {
-        printf("Brackets unbalanced!\n");
+    if (open != close) {                               
+        printf("Error brackets!\n");       // print error
         exit(-1);
     }
         
 }
 
-float calculation(char *array)                  // calculation
+float calculation(char *array)              // calculation
 {                                              
     int i = 0;                            // i --> selected element
                                              
     return second_priority(array, &i);  // call '+' && '-'
 }
  
-float second_priority(char *array, int *i)     // i --> selected element
+float second_priority(char *array, int *i)                      // i --> selected element
 {                                                 
-    float result = first_priority(array, i); // call '*' && '/'
-
-    while (*(array + *i) == '+' || *(array + *i) == '-') {                            
+    float result = first_priority(array, i);                  // call '*' && '/'
+                                                             
+    while (*(array + *i) == '+' || *(array + *i) == '-') {  //                            
         switch (*(array + *i)) {
         case '+':
             ++*i;
@@ -81,11 +81,7 @@ float first_priority(char *array, int *i)
             
             if (divider != 0) {                      
                 result /= divider;
-            } else {                        
-                printf("Division by zero is not defined\n");         
-                exit(-1);
-            }
-            
+            }            
             break;
        }
     }
@@ -97,12 +93,12 @@ float brackets_priority(char *array, int *i)
 {
     float result;
 
-    if (*(array + *i) == '(') {                   
+    if (*(array + *i) == '(') {            
         ++*i;
         result = second_priority(array, i);
         ++*i;
     } else {
-        result = digit(array, i);  
+        result = digit(array, i);
     }
     
     return result;
--- src/main.c
+++ src/main.c
@@ -10,15 +10,20 @@ int main(int argc, char *argv[])
 
 
     delete_space(array_char);  // array without space
+	/*Print function*/
+//	printf("\n%s = ",array_char);
     check_brackets(array_char);
+	/*Print function*/
+//	printf("\n%s = ",array_char);
     
-    int i=0;
-    while ((*(array_char+i)) != '\0'){
-	i++;
-    	printf("+");
+    int i=0;                             // i for while
+    while ((*(array_char+i)) != '\0'){  // change element
+	i++;                           
+    	printf("+");                  // print apperance
     }
-    printf("\n%s = ",array_char);
-    printf("%g\n", calculation(array_char)); // check value
+
+    printf("\n%s = ",array_char);             // print example without space 
+    printf("%g\n", calculation(array_char)); // calculation
     return 0;
 }
 
