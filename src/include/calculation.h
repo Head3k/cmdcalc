ddc908fbff1c34d8563d570399fcd7835e5480f5
He3k_Kubuntu <Nickolablaniy@mail.ru>
2020-06-04 15:51:16 +0700

    Update

--- src/include/calculation.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALCULATION_H
-#define CALCULATION_H
-
-double char_to_digit(char *string, int i1, int i2);
-double calculation(char *string, int start, int end);
-
-#endif
--- src/include/rework.h
+++
@@ -1,9 +0,0 @@
-#ifndef REWORK_H
-#define REWORK_H
-
-#include <stdio.h>
-#include <string.h>
-
-double string_rework(char *array_char, int string_length);
-
-#endif
